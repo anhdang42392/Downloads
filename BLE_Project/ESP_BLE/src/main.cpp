8e5f1b7ee8a18fa208f946641953deb8ffe6cbda
anhdang42392 <tuananh2017pro@gmail.com>
2024-11-01 22:30:05 +0700

    build BLE

--- BLE_Project/ESP_BLE/src/main.cpp
+++ BLE_Project/ESP_BLE/src/main.cpp
@@ -6,18 +6,127 @@
 #include <BLE2902.h>
 
 // put function declarations here:
-int myFunction(int, int);
+// int myFunction(int, int);
+// int result;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// void setup() {
+//   // put your setup code here, to run once:
+//   result = myFunction(2, 3);
+//   Serial.begin(9600);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+
+//   Serial.println(result);
+//   delay(1000);
+
+// }
+
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
+BLEDescriptor *pDescr;
+BLE2902 *pBLE2902;
+
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    deviceConnected = true;
+  };
+
+  void onDisconnect(BLEServer *pServer)
+  {
+    deviceConnected = false;
+  }
+};
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+          CHARACTERISTIC_UUID,
+          BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE |
+          BLECharacteristic::PROPERTY_INDICATE |
+          BLECharacteristic::PROPERTY_NOTIFY);
+
+  // Create a BLE Descriptor
+
+  pDescr = new BLEDescriptor((uint16_t)0x2901);
+  pDescr->setValue("A very interesting variable");
+  pCharacteristic->addDescriptor(pDescr);
+
+  pBLE2902 = new BLE2902();
+  pBLE2902->setNotifications(true);
+  pCharacteristic->addDescriptor(pBLE2902);
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0); // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // notify changed value
+  if (deviceConnected)
+  {
+    // float temp = 22.553;
+    // pCharacteristic->setValue(temp);
+    pCharacteristic->setValue(value); // gui gia tri
+    // std::string message = "Hello World";
+    // pCharacteristic->setValue(message);
+    
+    pCharacteristic->notify();
+    value++;
+    delay(1000);
+  }
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected)
+  {
+    delay(500);                  // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected)
+  {
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+  }
 }
